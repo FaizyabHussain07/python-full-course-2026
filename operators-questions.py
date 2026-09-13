0f1e0d117ec7d3510e0761fd7a9cb45c6402b6ca
FaizyabHussain07 <mfaizyabhussain@gmail.com>
2026-09-13 14:14:25 +0500

    q2

--- operators-questions.py
+++ operators-questions.py
@@ -1,13 +1,20 @@
 # q1 take 2 numbers from user and print their sum,difference,product,division and remainder
 
-num1= int(input("Enter number 1: "))
-num2= int(input("Enter number 2: "))
+# num1= int(input("Enter number 1: "))
+# num2= int(input("Enter number 2: "))
 
-# total = num1 + num2
-print(f"Sum: {num1 + num2}")
-print(f"Difference: {num1 - num2}")
-print(f"Product: {num1 * num2}")
-print(f"Division: {num1 / num2}")
-print(f"Remainder: {num1 % num2}")
+# # total = num1 + num2
+# print(f"Sum: {num1 + num2}")
+# print(f"Difference: {num1 - num2}")
+# print(f"Product: {num1 * num2}")
+# print(f"Division: {num1 / num2}")
+# print(f"Remainder: {num1 % num2}")
+
+
+# q2 taake a number input . print whether the number is even or odd using % and comparison operator
+
+# num = int(input("Enter number: "))
+
+# print(num % 2 == 0 )
 
 
