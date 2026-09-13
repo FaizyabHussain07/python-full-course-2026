b1c293ad7add9a87b6069c32dd18d094ddb7c78b
FaizyabHussain07 <mfaizyabhussain@gmail.com>
2026-09-13 14:09:07 +0500

    q1

---
+++ operators-questions.py
@@ -0,0 +1,13 @@
+# q1 take 2 numbers from user and print their sum,difference,product,division and remainder
+
+num1= int(input("Enter number 1: "))
+num2= int(input("Enter number 2: "))
+
+# total = num1 + num2
+print(f"Sum: {num1 + num2}")
+print(f"Difference: {num1 - num2}")
+print(f"Product: {num1 * num2}")
+print(f"Division: {num1 / num2}")
+print(f"Remainder: {num1 % num2}")
+
+
