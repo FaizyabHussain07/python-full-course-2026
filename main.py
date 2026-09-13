defadb9333dc7276dd1d50d1f34976501756f88e
FaizyabHussain07 <mfaizyabhussain@gmail.com>
2026-09-13 13:32:26 +0500

    first

---
+++ main.py
@@ -0,0 +1,56 @@
+name = "Faizyab"
+age = 17
+gender = "Male"
+
+print("Name : ",name)
+print("age : ",age)
+print("gender: ",gender)
+
+print (type(name))
+
+
+name = "Faizyab"
+age = 17
+gender = "Male"
+
+
+
+print("Name : ",name,end=" ")
+print("age : ",age)
+print("gender: ",gender)
+
+
+print (name,age,gender,sep="-")
+
+
+
+print(f"name: {name}, age: {age}, gender: {gender}")
+
+
+
+num1= "100"
+num2="200"
+
+print(int(num1) + int(num2))
+
+
+num1=100
+num2=200
+total = num1+num2
+print(f"total: {total}")
+
+
+
+number1= int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+total = number1 + number2
+print (f"You have enter first number: {number1}")
+
+print(f"You have enter second number: {number2}")
+
+
+print(f"total : {total}")
+
+
+print("hell      \t       o\n world")
+print("My name is \"Faizyab\" and i am 17 age")
