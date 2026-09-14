67be9d8f968ef1aa06b5973d93f0bb32b8de9564
FaizyabHussain07 <mfaizyabhussain@gmail.com>
2026-09-14 15:51:12 +0500

    condition q and loops

--- condition.py
+++ condition.py
@@ -95,3 +95,12 @@
 # else:
 #     print(f"{num1} and {num2} are equal")
 
+# leap year
+
+# year = int(input("Enter year: "))
+
+# if (year % 4 == 0 and year % 100 != 0 )or year % 400 == 0:
+#     print(f"{year} is a leap year")
+# else:
+#     print("It is not leap year")
+
---
+++ loop.py
@@ -0,0 +1,104 @@
+# # print heloo 10 time
+# i = 1
+
+# while i <= 5:
+#     print("hello")
+#     i += 1
+
+# print(i)
+
+
+
+# 1-10 print 
+# 1-n print
+
+# n = int(input("Enter number: "))
+
+# i = 1
+
+# while i <= n:
+#     print(i ,end=" ")
+#     i+=1
+
+#  star and end by users 
+
+
+# start = int(input("Enter start number: "))
+# end = int(input("enter end number: "))
+
+# i = start
+
+# while i <= end:
+#     print(i,end=" ")
+#     i += 1
+
+# print(f"\n After ending loop , start value is: {start}")
+
+
+#  start to end print even 
+
+# start = int(input("Enter start number: "))
+# end = int(input("enter end number: "))
+
+# i = start 
+
+# while i <=end:
+#     if i % 2 == 0:
+#         print(i, end=" ")
+#     i+=1    
+    
+# print start to end  which are divisible by 3 and 4
+
+# start = int(input("Enter start number: "))
+# end = int(input("enter end number: "))
+
+# i = start 
+
+# while i <= end:
+#     if i %3==0 and i%4==0:
+#         print(i,end=" ")
+#     i +=1    
+
+
+#  end to start 10-1
+
+# start = int(input("Enter start number: "))
+# end = int(input("enter end number: "))
+
+# i = 10
+
+# while i>=1:
+#     print(i,end=" ")
+#     i-=1
+
+
+# summ of 1-100
+
+# start = int(input("Enter start number: "))
+# end = int(input("enter end number: "))
+
+
+# i =start
+# total=0
+
+# while i <=end:
+#     total = total+i
+#     i +=1
+
+# print(f"total= {total}")
+
+
+
+# start = int(input("Enter start number: "))
+# end = int(input("enter end number: "))
+
+
+# i =start
+# total=0
+
+# while i <=end:
+#     if i %2==0 and i%7==0:
+#         total = total+i
+#     i +=1
+
+# print(f"total= {total}")
