7ac3acdd94a1f6e75f6dd55c0a8d15b0c33e9d55
FaizyabHussain07 <mfaizyabhussain@gmail.com>
2026-09-14 09:37:09 +0500

    operator

--- operators-questions.py
+++ operators-questions.py
@@ -18,3 +18,27 @@
 # print(num % 2 == 0 )
 
 
+# q3 take the user age as input and print whether the user is eligible for voting age>=18 and  whether the user is eligible for senior citezenship age>=60
+# prnt result
+
+
+# age = int(input("Enter your age: "))
+# can_vote = age >=18
+# senior_citizen = age >= 60 
+
+
+# print(f"user can vote: {can_vote}")
+# print(f"user is senior citizen : {senior_citizen}")
+
+
+# q4 a student score marks in 3 sub tak all  three as input , calculate the total and average  and print both using an f string
+
+
+sub1 = int(input("Enter marks for subject 1: "))
+sub2 = int(input("Enter marks for subject 2: "))
+sub3= int(input("Enter marks for subject 3: "))
+
+total = sub1+sub2+sub3
+avg= total/3
+
+print(f"Total marks: {total}, Average marks: {avg:.2f}")
