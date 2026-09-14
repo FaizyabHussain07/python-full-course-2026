6551d084e7254bec16b9b61ec6e0b63c2d5f7efd
FaizyabHussain07 <mfaizyabhussain@gmail.com>
2026-09-14 10:29:53 +0500

    conditions

---
+++ condition.py
@@ -0,0 +1,97 @@
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print("you can vote")
+# else:
+#     print("you cannot vote")
+
+# # print("done")
+
+# pyhs = int (input("Enter your pyhsics number: "))
+# com = int(input("Enter your computer number: "))
+
+# if pyhs >= 33 and com >= 33:
+#     print("you are pass")
+# elif pyhs < 33 and com >= 33:
+#     print("you are aprox pass")
+# else:
+#     print("you are fail")
+
+
+"""
+90 above a
+81 - 90 b
+71-80 c
+61 - 70 d
+60 below f
+
+"""
+
+# marks = int (input("Enter your marks: "))
+
+# if marks >= 91 and marks <= 100:
+#     print("A")
+# elif marks >= 81 and marks <= 90:
+#     print("B")
+# elif marks >= 71 and marks <= 80:
+#     print("C")
+# elif marks >= 61 and marks <= 70:
+#     print("D")
+# elif marks >= 0 and marks <= 60:
+#     print("F")
+# else:
+#     print("Invalid marks")
+
+
+
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     cert = int(input("do you have cert enter 1 for true 0 for false: "))
+#     if cert == 1:
+#         print("you are hired")
+#     else:
+#         print("you have not certificate, you are not hired")
+# else:
+#     print("you are not hired because of age")
+
+
+# age = int(input("Enter your age: "))
+
+# # if age >= 18:
+# #     status = "adult"
+# # else:
+# #     status = "minor"
+
+# status = "adult " if age >= 18 else "minor"
+
+# print(f"your status is: {status}")
+
+
+
+
+# questions 
+
+# num = int(input("Enter numbers: "))
+
+# if num > 0:
+#     print("positive")
+# elif num <0:
+#     print("negative ")
+# else:
+#     print("zero")
+
+
+
+# questionm
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# if num1 >num2:
+#     print(f"{num1} is greater than {num2}")
+# elif num2>num1:
+#     print(f"{num2} is greater than {num1}")
+# else:
+#     print(f"{num1} and {num2} are equal")
+
--- operators-questions.py
+++ operators-questions.py
@@ -34,11 +34,11 @@
 # q4 a student score marks in 3 sub tak all  three as input , calculate the total and average  and print both using an f string
 
 
-sub1 = int(input("Enter marks for subject 1: "))
-sub2 = int(input("Enter marks for subject 2: "))
-sub3= int(input("Enter marks for subject 3: "))
+# sub1 = int(input("Enter marks for subject 1: "))
+# sub2 = int(input("Enter marks for subject 2: "))
+# sub3= int(input("Enter marks for subject 3: "))
 
-total = sub1+sub2+sub3
-avg= total/3
+# total = sub1+sub2+sub3
+# avg= total/3
 
-print(f"Total marks: {total}, Average marks: {avg:.2f}")
+# print(f"Total marks: {total}, Average marks: {avg:.2f}")
